86b2f182bb2dfcb899d0e7a44b550e6c83dc4da4
vitamina <lucid2713@gmail.com>
2022-08-25 17:14:12 +0900

    update0825-1

--- wheel.js
+++ wheel.js
@@ -1,12 +1,44 @@
-function zoom(event) {
-  event.preventDefault();
-  scale += event.deltaY * -0.01;
-  // Restrict scale
-  scale = Math.min(Math.max(0.5, scale), 5);
-  // Apply scale transform
-  el.style.transform = `scale(${scale})`;
-}
 
-let scale = 1;
-const el = document.querySelector('#particle-nav');
-el.onwheel = zoom;
+
+ var scale = 1,
+        panning = false,
+        pointX = 0,
+        pointY = 0,
+        start = { x: 0, y: 0 },
+        zoom = document.getElementById("particle-nav");
+
+      function setTransform() {
+        zoom.style.transform = "translate(" + pointX + "px, " + pointY + "px) scale(" + scale + ")";
+      }
+
+      zoom.onmousedown = function (e) {
+        e.preventDefault();
+        start = { x: e.clientX - pointX, y: e.clientY - pointY };
+        panning = true;
+      }
+
+      zoom.onmouseup = function (e) {
+        panning = false;
+      }
+
+      zoom.onmousemove = function (e) {
+        e.preventDefault();
+        if (!panning) {
+          return;
+        }
+        pointX = (e.clientX - start.x);
+        pointY = (e.clientY - start.y);
+        setTransform();
+      }
+
+      zoom.onwheel = function (e) {
+        e.preventDefault();
+        var xs = (e.clientX - pointX) / scale,
+          ys = (e.clientY - pointY) / scale,
+          delta = (e.wheelDelta ? e.wheelDelta : -e.deltaY);
+        (delta > 0) ? (scale *= 1.2) : (scale /= 1.2);
+        pointX = e.clientX - xs * scale;
+        pointY = e.clientY - ys * scale;
+
+        setTransform();
+      }
